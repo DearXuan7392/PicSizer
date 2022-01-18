b899c6b4c9465fe96355e5865425791a90c7fd34
dearxuan <1320699768@qq.com>
2022-01-18 21:36:46 +0800

    修改版本号

--- PicSizer/Class/Static/Info.cs
+++ PicSizer/Class/Static/Info.cs
@@ -40,8 +40,8 @@ namespace PicSizer
         {
             mainVersion = 4,
             secondVersion = 2,
-            thirdVersion = 0,
-            alpha = false
+            thirdVersion = 1,
+            alpha = true
         };
 
         /// <summary>
