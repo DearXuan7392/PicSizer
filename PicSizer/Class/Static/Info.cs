9748c61073c702b91bc04ec20e11333191f991f3
DearXuan7392 <1320699768@qq.com>
2022-03-12 12:50:39 +0800

    4.8.1 update

--- PicSizer/Class/Static/Info.cs
+++ PicSizer/Class/Static/Info.cs
@@ -34,8 +34,8 @@ namespace PicSizer.Class.Static
         public static readonly Unit.Version ProjectVersion = new Unit.Version()
         {
             mainVersion = 4,
-            secondVersion = 7,
-            thirdVersion = 2,
+            secondVersion = 8,
+            thirdVersion = 1,
             alpha = true
         };
 
