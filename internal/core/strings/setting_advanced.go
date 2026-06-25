f0a58d0b81008e938d6deb00d5be3a7f159f8231
Xuan <mail@dearxuan.com>
2026-06-25 23:01:11 +0800

    调整了"为索引编码预留透明像素"选项的描述文本与Tooltip

--- internal/core/strings/setting_advanced.go
+++ internal/core/strings/setting_advanced.go
@@ -6,5 +6,5 @@ var (
 	TextAdvPNGSettings      = "PNG 设置"
 	TextAdvWebPSettings     = "WebP 设置"
 	TextAdvFineQuality      = "精细化画质"
-	TextPngKeepIndexedAlpha = "为索引(调色盘)编码预留透明像素"
+	TextPngKeepIndexedAlpha = "为索引编码预留透明像素"
 )
