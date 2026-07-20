d4c1a6f24cca3fadf1e47dbd3870495e29013621
DearXuan <mail@dearxuan.com>
2026-07-20 16:34:53 +0800

    修改输出方式的文本描述

--- internal/core/strings/setting_format.go
+++ internal/core/strings/setting_format.go
@@ -11,7 +11,7 @@ var (
 	TextFilenameTpl  = "文件名模板"
 	TextStartIndex   = "起始下标"
 
-	TipExtension   = "当选择原格式时, 若图片实际格式与后缀名不同, 会以后缀名为准, 自动调整编码方式"
+	TipExtension   = "当选择原格式时, 若图片实际格式与后缀名不同, 会以后缀名为准, 自动调整编码方式\nBMP 格式暂不支持压缩, 仅支持输出最佳画质"
 	TipFilenameTpl = "文件名模板 (不含后缀), 替换原则如下:\n      {name} 被替换为原始文件名, 如 abc.jpg 中, {name} 被替换为 abc\n      {id} 被替换为程序自动分配的索引数字\n后缀会根据上方\"输出格式\"自动追加, 无需在模板中填写"
 	TipStartIndex  = "输出文件的起始序号, 即文件名模板中 {id} 的起始值"
 )
--- internal/core/strings/setting_output.go
+++ internal/core/strings/setting_output.go
@@ -3,10 +3,10 @@ package strs
 // 输出方式
 var (
 	TextOutputMode   = "输出方式"
+	TextOutputDir    = "输出到单独文件夹"
 	TextCoverOrigin  = "覆盖源文件"
-	TextOutputDir    = "输出到目录"
-	TextOutputStruct = "保留目录结构"
+	TextOutputStruct = "输出目录结构"
 	TextOutputPath   = "输出目录"
 
-	TipOutputType = "输出到目录: \n      将图片全部输出到单个文件夹内\n覆盖源文件: \n      直接覆盖源文件, 需做好备份\n保留目录结构: \n      将文件按原始的相对位置输出到新的文件夹中"
+	TipOutputType = TextOutputDir + ": \n      将图片全部输出到单个文件夹内\n" + TextCoverOrigin + ": \n      直接覆盖源文件, 需做好备份\n" + TextOutputStruct + ": \n      将文件按原始的相对位置输出到新的文件夹中"
 )
